3feaa1350c6d0ea8e278209752f4fc2d23d03357
d-shlenskii <shlenskii@2A2I-PRO-32QGNF.local>
2024-10-24 20:53:26 +0300

    save buffer upd

--- utils/save.py
+++ utils/save.py
@@ -6,20 +6,21 @@ from utils.types import Buffer
 
 
 def save_buffer(state: Buffer, path: str):
+    data_dict = {k: v for k, v in state.items()}
     with open(path, "w") as file:
-        json.dump(state.experience, file)
+        json.dump(data_dict, file)
 
 def load_buffer(state: Buffer, path: str):
     with open(path) as file:
-        experience = json.load(file)
-    if state.experience.keys() == experience.keys():
-        for k, v in experience.items():
-            state.experience[k] = v
+        data_dict = json.load(file)
+    if state["experience"].keys() == data_dict["experience"].keys():
+        for k, v in data_dict["experience"].items():
+            state["experience"][k] = v
     else:
         warnings.warn(
             "Given data is incompatible with the Buffer!\n" +
-            f"Buffer fiels: {', '.join(sorted(list(state.experience.key())))}\n" +
-            f"Data fiels: {', '.join(sorted(list(experience.key())))}"
+            f"Buffer fields: {', '.join(sorted(list(state["experience"].keys())))}\n" +
+            f"Data fields: {', '.join(sorted(list(data_dict["experience"].keys())))}"
         )
 
     return state
