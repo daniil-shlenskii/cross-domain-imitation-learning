e1a8c9d65f5bec14a2c88a79f19ef974a2931f37
d-shlenskii <shlenskii@2A2I-PRO-32QGNF.local>
2024-10-24 20:43:42 +0300

    buffer saving utility added

---
+++ utils/save.py
@@ -0,0 +1,25 @@
+import warnings
+
+import json
+
+from utils.types import Buffer
+
+
+def save_buffer(state: Buffer, path: str):
+    with open(path, "w") as file:
+        json.dump(state.experience, file)
+
+def load_buffer(state: Buffer, path: str):
+    with open(path) as file:
+        experience = json.load(file)
+    if state.experience.keys() == experience.keys():
+        for k, v in experience.items():
+            state.experience[k] = v
+    else:
+        warnings.warn(
+            "Given data is incompatible with the Buffer!\n" +
+            f"Buffer fiels: {', '.join(sorted(list(state.experience.key())))}"
+            f"Data fiels: {', '.join(sorted(list(experience.key())))}"
+        )
+
+    return state
