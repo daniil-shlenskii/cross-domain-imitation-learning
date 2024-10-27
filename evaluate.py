3b6b421bc5853e5f507a619b505b9c7f8e54d36a
d-shlenskii <shlenskii@2A2I-PRO-32QGNF.local>
2024-10-27 14:05:22 +0300

    evaluation functions

---
+++ evaluate.py
@@ -0,0 +1,17 @@
+from typing import Dict
+
+import gymnasium as gym
+import numpy as np
+
+from experts.base_agent import Agent
+
+
+def evaluate(agent: Agent, env: gym.Env, num_episodes: int) -> Dict[str, float]:
+    env = gym.wrappers.RecordEpisodeStatistics(env, deque_size=num_episodes)
+    for _ in range(num_episodes):
+        observation, _, done = *env.reset(), False
+        while not done:
+            action = agent.sample_actions(observation)
+            observation, _, done, _, _ = env.step(action)
+
+    return {"return": np.mean(env.return_queue), "length": np.mean(env.length_queue)}
