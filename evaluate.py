fb3d7a9c3215cda0a551e19108c9a3f51b990715
d-shlenskii <shlenskii@2A2I-PRO-32QGNF.local>
2024-10-29 13:26:39 +0300

    considering truncation while evaulating

--- evaluate.py
+++ evaluate.py
@@ -9,9 +9,10 @@ from experts.base_agent import Agent
 def evaluate(agent: Agent, env: gym.Env, num_episodes: int) -> Dict[str, float]:
     env = gym.wrappers.RecordEpisodeStatistics(env, buffer_length=num_episodes)
     for _ in range(num_episodes):
-        observation, _, done = *env.reset(), False
-        while not done:
+        observation, _, done, truncated = *env.reset(), False, False
+        while not (done or truncated):
             action = agent.sample_actions(observation)
-            observation, _, done, _, _ = env.step(action)
+            observation, _, done, truncated, _ = env.step(action)
+
 
     return {"return": np.mean(env.return_queue), "length": np.mean(env.length_queue)}
