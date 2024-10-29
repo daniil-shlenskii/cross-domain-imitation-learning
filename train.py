404d3d7ab86b959a21c2916b060120bd8e9fa48f
d-shlenskii <shlenskii@2A2I-PRO-32QGNF.local>
2024-10-29 13:25:18 +0300

    args parsing; logging; buffer storage

--- train.py
+++ train.py
@@ -1,5 +1,7 @@
+import argparse
+from pprint import pformat
+
 import jax
-import jax.numpy as jnp
 
 import numpy as np
 
@@ -16,10 +18,26 @@ from evaluate import evaluate
 from utils.save import save_buffer, load_buffer
 
 
-def main():
-    wandb.init(project="test_jax_rl")
+TMP_RANDOM_BUFFER_STORAGE_DIR = "_tmp_data_storage"
 
-    config = OmegaConf.load("config.yaml")
+
+def init() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="RL agent training script"
+    )
+    parser.add_argument("--config_path",        type=str)
+    parser.add_argument("--wandb_project_name", type=str,  default="default_wandb_project_name")
+    parser.add_argument("--save_random_buffer", type=bool, default=True)
+    return parser.parse_args()
+
+
+def main(args):
+    wandb.init(project=args.wandb_project_name)
+
+    config = OmegaConf.load(args.config_path)
+
+    # logger.info(f"CONFIG:\n{pformat(dict(config), indent=4)}")
+    logger.info(f"\nCONFIG:\n-----\n{OmegaConf.to_yaml(config)}")
 
     # reprodicibility
     rng = jax.random.PRNGKey(config.seed)
@@ -27,9 +45,6 @@ def main():
     # environment init
     env = instantiate(config.environment)
     eval_env = instantiate(config.evaluation.environment)
-
-    print(f"{env.action_space.low = }")
-    print(f"{env.action_space.high = }")
 
     # buffer init
     observation, _ = env.reset()
@@ -56,7 +71,7 @@ def main():
     )
 
     # buffer collection
-    def do_environment_step(action):
+    def do_environment_step(action, i):
         nonlocal env, state, observation
     
         # do step in the environment
@@ -78,47 +93,61 @@ def main():
 
         # update env if terminated
         if done or truncated:
-            observation, _ = env.reset()
+            observation, _ = env.reset(seed=config.seed + i)
 
-    # download buffer if needed
+    # download random buffer if needed
     n_iters_collect_buffer = config.n_iters_collect_buffer
-    precollected_data_dir = Path(config.get("precollected_data_dir", "tmp_data_storage"))
-    precollected_data_path = precollected_data_dir / f"{config.env_name}.pickle"
-    precollected_data_dir.mkdir(exist_ok=True)
-    if precollected_data_path.exists():
-        state = load_buffer(state, precollected_data_path)
+
+    load_random_buffer_dir = Path(config.get("load_random_buffer_dir", TMP_RANDOM_BUFFER_STORAGE_DIR))
+    load_random_buffer_path = load_random_buffer_dir / f"{config.env_name}.pickle"
+    load_random_buffer_dir.mkdir(exist_ok=True)
+    if load_random_buffer_path.exists():
+        state = load_buffer(state, load_random_buffer_path)
         n_iters_collect_buffer -= state.current_index
         n_iters_collect_buffer = max(0, n_iters_collect_buffer)
+
+        logger.info(f"Loading Random Buffer from {load_random_buffer_path}")
         logger.info(f"{state.current_index} samples already collected. {n_iters_collect_buffer} are left.")
 
     # collect the rest of the data
-    observation, _  = env.reset(seed=config.seed)
-    for _ in tqdm(range(n_iters_collect_buffer)):
-        action = env.action_space.sample()
-        do_environment_step(action)
+    logger.info("Random Buffer collecting.")
 
-    if config.store_random_buffer:
-        save_buffer(state, precollected_data_path, logger)
+    observation, _  = env.reset(seed=config.seed)
+    for i in tqdm(range(n_iters_collect_buffer)):
+        action = env.action_space.sample()
+        do_environment_step(action, i)
+
+    logger.info("Random Buffer collected.")
+
+    # save random buffer
+    if args.save_random_buffer:
+        save_random_buffer_dir = Path(config.get("save_random_buffer_dir", TMP_RANDOM_BUFFER_STORAGE_DIR))
+        save_random_buffer_path = save_random_buffer_dir / f"{config.env_name}.pickle"
+        save_random_buffer_dir.mkdir(exist_ok=True)
+        save_buffer(state, save_random_buffer_path)
+        logger.info(f"Random Buffer is stored under the following path: {save_random_buffer_path}")
 
     # training
+    logger.info("Training..")
+
     observation, _  = env.reset(seed=config.seed)
     for i in tqdm(range(config.n_iters_training)):
         # sample actions
         action = agent.sample_actions(observation[None])
 
         # do step in the environment
-        do_environment_step(action[0])
+        do_environment_step(action[0], i)
 
         # do RL optimization step
         rng, key = jax.random.split(rng)
         batch = buffer.sample(state, key).experience
         update_info = agent.update(batch)
 
-        if i % config.log_every == 0:
+        if (i + 1) % config.log_every == 0:
             for k, v in update_info.items():
                 wandb.log({f"training/{k}": v}, step=i)
 
-        if i % config.eval_every == 0:
+        if (i + 1) % config.eval_every == 0:
             eval_info = evaluate(agent, eval_env, num_episodes=config.evaluation.num_episodes)
             for k, v in eval_info.items():
                 wandb.log({f"evaluation/{k}": v}, step=i)
@@ -126,4 +155,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    args = init()
+    main(args)
