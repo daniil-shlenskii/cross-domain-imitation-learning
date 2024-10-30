39f85819a84fc73d97320b7aad9de61c5e8d07f3
d-shlenskii <shlenskii@2A2I-PRO-32QGNF.local>
2024-10-30 17:52:52 +0300

    evaluation reproducibilty enabled

--- evaluate.py
+++ evaluate.py
@@ -6,13 +6,12 @@ import numpy as np
 from experts.base_agent import Agent
 
 
-def evaluate(agent: Agent, env: gym.Env, num_episodes: int) -> Dict[str, float]:
+def evaluate(agent: Agent, env: gym.Env, num_episodes: int, seed: int=0) -> Dict[str, float]:
     env = gym.wrappers.RecordEpisodeStatistics(env, buffer_length=num_episodes)
-    for _ in range(num_episodes):
-        observation, _, done, truncated = *env.reset(), False, False
+    for i in range(num_episodes):
+        observation, _, done, truncated = *env.reset(seed=seed+i), False, False
         while not (done or truncated):
-            action = agent.sample_actions(observation)
+            action = agent.eval_actions(observation)
             observation, _, done, truncated, _ = env.step(action)
-
 
     return {"return": np.mean(env.return_queue), "length": np.mean(env.length_queue)}
