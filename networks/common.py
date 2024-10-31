fc35845a4962b37b75d60f119dcaad1891214554
d-shlenskii <shlenskii@2A2I-PRO-32QGNF.local>
2024-10-31 15:51:18 +0300

    mlp: activate_final option added

--- networks/common.py
+++ networks/common.py
@@ -1,5 +1,7 @@
 from typing import Callable, Optional, Sequence
 
+import jax
+
 import flax.linen as nn
 import jax.numpy as jnp
 
@@ -7,20 +9,24 @@ import jax.numpy as jnp
 def default_init(scale: Optional[float] = jnp.sqrt(2)):
     return nn.initializers.orthogonal(scale)
 
+
 class MLP(nn.Module):
     hidden_dims: Sequence[int]
     activation: Callable[[jnp.ndarray], jnp.ndarray] = nn.relu
+    activate_final: Optional[bool] = False
     dropout_rate: Optional[float] = None
 
     @nn.compact
     def __call__(
         self, x: jnp.ndarray, train: bool=False
     ) -> jnp.ndarray:
-        for hid_dim in self.hidden_dims:
+        for i, hid_dim in enumerate(self.hidden_dims):
             x = nn.Dense(hid_dim, kernel_init=default_init())(x)
-            x = self.activation(x)
-            if self.dropout_rate is not None:
-                x = nn.Dropout(reate=self.dropout_rate)(
-                    x, deterministic=not train
-                )
+
+            if i < len(self.hidden_dims) - 1 or self.activate_final:
+                x = self.activation(x)
+                if self.dropout_rate is not None:
+                    x = nn.Dropout(reate=self.dropout_rate)(
+                        x, deterministic=not train
+                    )
         return x
