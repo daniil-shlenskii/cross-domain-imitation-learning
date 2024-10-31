7113ede83d485811c4bb40ea25eb69b2766be626
d-shlenskii <shlenskii@2A2I-PRO-32QGNF.local>
2024-10-31 21:02:04 +0300

    gail draft

--- agents/gail/gail_agent.py
+++ agents/gail/gail_agent.py
@@ -2,11 +2,12 @@ import jax
 
 import gymnasium as gym
 
+from hydra.utils import instantiate
 from omegaconf.dictconfig import DictConfig
 
 from agents.base_agent import Agent
 from nn.train_state import TrainState
-from utils.types import DataType, PRNGKey
+from utils.types import DataType, PRNGKey, Buffer
 
 
 class GAILAgent(Agent):
@@ -15,7 +16,7 @@ class GAILAgent(Agent):
         *
         seed: int,
         observations_space: gym.Space,
-        actions_space: gym.Space,
+        action_space: gym.Space,
         #
         expert_buffer: Buffer,
         #
@@ -26,18 +27,40 @@ class GAILAgent(Agent):
         rng = jax.random.key(seed)
         self._rng = rng
 
-        # agent initialization
-        
-  
+        # agent and discriminator init
+        self.agent = instantiate(agent_config, observations_space=observations_space, action_space=action_space)
+        self.discriminator = instantiate(discriminator_config)
+
+        # 
+        self.seed = seed
+        self.expert_buffer = expert_buffer
+
+    @property
+    def actor(self):
+        return self.agent.actor
+
     def update(self, batch: DataType):
         ...
 
-
 @jax.jit
 def _update_jit(
-    batch: DataType,
     rng: PRNGKey,
+    batch: DataType,
+    expert_batch: DataType,
     #
     agent: Agent,
-    discriminator: TrainState,
+    disc: GAILDiscriminator,
 ):
+    batch["rewards"] = disc.get_rewards(batch["observations"], batch["observations_next"])
+    new_agent, agent_info, agent_stats_info = agent.update(batch)
+    new_disc, disc_info, disc_stats_info = disc.update(expert_batch)
+
+    info = {**agent_info, **disc_info}
+    stats_info = {**agent_stats_info, **disc_stats_info}
+    return (
+        rng,
+        new_agent,
+        new_disc,
+        info,
+        stats_info,
+    )
