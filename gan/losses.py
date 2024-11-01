a5f691cf97841bdaf47b6a1192bc064332c72431
d-shlenskii <shlenskii@2A2I-PRO-32QGNF.local>
2024-11-01 16:30:21 +0300

    gan losses added

---
+++ gan/losses.py
@@ -0,0 +1,29 @@
+from typing import Callable
+
+import jax
+import jax.numpy as jnp
+
+from gan.discriminator import Discriminator
+from utils.types import PRNGKey
+
+
+def d_logistic_loss(real_logits: jnp.ndarray, fake_logits: jnp.ndarray):
+    real_loss = jax.nn.softplus(-real_logits)
+    fake_loss = jax.nn.softplus(fake_logits)
+    return real_loss.mean() + fake_loss.mean()
+
+def g_nonsaturating_loss(fake_logits: jnp.ndarray):
+    loss = jax.nn.softplus(-fake_logits) 
+    return loss
+
+def gradient_penalty(
+    key: PRNGKey,
+    real_batch: jnp.ndarray,
+    fake_batch: jnp.ndarray,
+    discriminator_grad_fn: Callable,
+):
+    t = jax.random.uniform(key, shape=(real_batch.shape[0], 1))
+    interpolated_batch = real_batch * t + fake_batch * (1 - t)
+    grads = jax.vmap(discriminator_grad_fn)(interpolated_batch)
+    norms = jax.vmap(jnp.linalg.norm)(grads)
+    return norms.mean()
