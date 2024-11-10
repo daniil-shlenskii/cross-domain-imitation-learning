215f0843e9c49ad6c326bfd5b1fdf39a079d8c42
d-shlenskii <shlenskii@2A2I-PRO-32QGNF.local>
2024-11-10 16:06:25 +0300

    dida: process_policy_discriminator_input fixed

--- agents/dida/utils.py
+++ agents/dida/utils.py
@@ -5,7 +5,7 @@ import jax.numpy as jnp
 @jax.jit
 def process_policy_discriminator_input(x: jnp.ndarray):
     doubled_b_size, dim = x.shape
-    x = x.reshape(2, doubled_b_size // 2, dim).transpose(1, 2, 0).reshape(-1, dim * 2)
+    x = x.reshape(2, doubled_b_size // 2, dim).transpose(1, 0, 2).reshape(-1, dim * 2)
     return x
 
 @jax.jit
