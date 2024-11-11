5949111dc4066dc13e18ba8a4737756da02df044
d-shlenskii <shlenskii@2A2I-PRO-32QGNF.local>
2024-11-11 16:54:24 +0300

    agent evaluation method added

--- agents/base_agent.py
+++ agents/base_agent.py
@@ -49,6 +49,16 @@ class Agent(PyTreeNode, SaveLoadFrozenDataclassMixin):
 
     def _preprocess_observations(self, observations: np.ndarray) -> np.ndarray:
         return observations
+    
+    def evaluate(self, env: gym.Env, num_episodes: int, seed: int=0) -> Dict[str, float]:
+        env = gym.wrappers.RecordEpisodeStatistics(env, buffer_length=num_episodes)
+        for i in range(num_episodes):
+            observation, _, done, truncated = *env.reset(seed=seed+i), False, False
+            while not (done or truncated):
+                action = self.eval_actions(observation)
+                observation, _, done, truncated, _ = env.step(action)
+
+        return {"return": np.mean(env.return_queue), "length": np.mean(env.length_queue)}
 
 @jax.jit
 def _sample_actions_jit(key: PRNGKey, actor: TrainState, observations: np.ndarray) -> Tuple[PRNGKey, jnp.ndarray]:
