7548826e868cfa7c40e4b39730b0f5729173b24b
d-shlenskii <shlenskii@2A2I-PRO-32QGNF.local>
2024-11-13 12:57:11 +0300

    dada: update gail step fixed: agent with learner batch; discriminator with mixed batch

--- agents/dida/dida_agent.py
+++ agents/dida/dida_agent.py
@@ -314,6 +314,7 @@ def _update_jit(
         encoded_expert_policy_batch=encoded_expert_policy_batch,
         expert_encoder=expert_encoder,
     )
+
     if use_das:
         alpha, sar_info = self_adaptive_rate(
             domain_discriminator=domain_discriminator,
