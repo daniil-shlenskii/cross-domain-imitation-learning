7d4e772fec3b2026933959090c8c19aaf8f123b4
d-shlenskii <daniil.shlenskii@gmail.com>
2024-11-14 16:36:04 +0300

    gan fixed: fake_batch key to generations key

--- gan/gan.py
+++ gan/gan.py
@@ -72,7 +72,7 @@ def _update_jit(
     discriminator: Discriminator,
 ):
     new_gen, gen_info, gen_stats_info = generator.update(batch=gan_inputs, discriminator=discriminator)
-    new_disc, disc_info, disc_stats_info = discriminator.update(real_batch=real_batch, fake_batch=gen_info.pop("fake_batch"))
+    new_disc, disc_info, disc_stats_info = discriminator.update(real_batch=real_batch, fake_batch=gen_info.pop("generations"))
 
     info = {**gen_info, **disc_info}
     stats_info = {**gen_stats_info, **disc_stats_info}
