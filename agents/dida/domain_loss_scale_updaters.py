737d5f8bcc2577547175d62d54b0cb81100d225b
Бобрин Максим <bobrin@airi.net>
2024-11-20 11:33:41 +0000

    domain loss scale range updater updated

--- agents/dida/domain_loss_scale_updaters.py
+++ agents/dida/domain_loss_scale_updaters.py
@@ -15,12 +15,17 @@ class IdentityDomainLossScaleUpdater(abc.ABC):
 class RangeDomainLossScaleUpdater:
     def __init__(
         self,
-        update_range: float=0.05,
-        factor: float=0.5,
+        update_range: float = 0.03,
+        unlock_range: float = 0.1,
+        factor: float = 0.5,
         min_clip: float = 0.1,
     ):
         self.update_range = update_range
-        self.factor = factor
+        self.unlock_range = unlock_range
+
+        self.update_factor = 0.1
+        self.unlock_factor = factor / self.update_factor
+
         self.min_clip = min_clip
         self._updated_recently = False
 
@@ -30,10 +35,14 @@ class RangeDomainLossScaleUpdater:
             0.5 - self.update_range <= dida_agent.p_acc_ema < 0.5 + self.update_range
         )
         if self._updated_recently:
-            if not update_condition:
+            unlock_condition = not (
+                0.5 - self.unlock_range <= dida_agent.p_acc_ema < 0.5 + self.unlock_range
+            )
+            if unlock_condition:
                 self._updated_recently = False
+                domain_loss_scale = domain_loss_scale * self.unlock_factor
         else:
             if update_condition:
-                domain_loss_scale = domain_loss_scale * self.factor
+                domain_loss_scale = domain_loss_scale * self.update_factor
                 self._updated_recently = True
         return np.clip(domain_loss_scale, self.min_clip, None)
