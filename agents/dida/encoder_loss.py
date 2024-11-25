3d1c500b7c6af0fa56b50c5aa277cf9acacf0d27
d-shlenskii <daniil.shlenskii@gmail.com>
2024-11-25 15:29:18 +0000

    dida encoders losses: united logic

--- agents/dida/encoder_loss.py
+++ agents/dida/encoder_loss.py
@@ -1,3 +1,6 @@
+import functools
+from typing import Callable
+
 import jax
 import jax.numpy as jnp
 from gan.base_losses import (g_nonsaturating_logistic_loss,
@@ -7,24 +10,27 @@ from nn.train_state import TrainState
 from utils.types import DataType, Params
 
 
-def learner_encoder_loss(
+def _encoder_loss(
     params: Params,
     state: TrainState,
     batch: DataType,
     policy_discriminator: Discriminator,
     domain_discriminator: Discriminator,
     domain_loss_scale: float,
+    #
+    policy_loss_fn: Callable,
+    domain_loss_fn: Callable,
 ):
     batch["observations"] = state.apply_fn({"params": params}, batch["observations"], train=True)
     batch["observations_next"] = state.apply_fn({"params": params}, batch["observations_next"], train=True)
 
     policy_batch = jnp.concatenate([batch["observations"], batch["observations_next"]], axis=1)
     policy_logits = policy_discriminator(policy_batch)
-    policy_loss = g_nonsaturating_softplus_loss(-policy_logits)
+    policy_loss = policy_loss_fn(-policy_logits)
 
     domain_batch = batch["observations"]
     domain_logits = domain_discriminator(domain_batch)
-    domain_loss = -g_nonsaturating_logistic_loss(domain_logits)
+    domain_loss = domain_loss_fn(domain_logits)
 
     loss = policy_loss + domain_loss_scale * domain_loss
     info = {
@@ -35,30 +41,14 @@ def learner_encoder_loss(
     }
     return loss, info
 
-def expert_encoder_loss(
-    params: Params,
-    state: TrainState,
-    batch: DataType,
-    policy_discriminator: Discriminator,
-    domain_discriminator: Discriminator,
-    domain_loss_scale: float,
-):
-    batch["observations"] = state.apply_fn({"params": params}, batch["observations"], train=True)
-    batch["observations_next"] = state.apply_fn({"params": params}, batch["observations_next"], train=True)
+learner_encoder_loss = functools.partial(
+    _encoder_loss,
+    policy_loss_fn=lambda policy_logits: g_nonsaturating_softplus_loss(-policy_logits),
+    domain_loss_fn=lambda domain_logits: -g_nonsaturating_logistic_loss(domain_logits)
+)
 
-    policy_batch = jnp.concatenate([batch["observations"], batch["observations_next"]], axis=1)
-    policy_logits = policy_discriminator(policy_batch)
-    policy_loss = g_nonsaturating_softplus_loss(policy_logits)
-
-    domain_batch = batch["observations"]
-    domain_logits = domain_discriminator(domain_batch)
-    domain_loss = g_nonsaturating_logistic_loss(domain_logits)
-
-    loss = policy_loss + domain_loss_scale * domain_loss
-    info = {
-        f"{state.info_key}_loss": loss,
-        f"{state.info_key}_policy_loss": policy_loss,
-        f"{state.info_key}_domain_loss": domain_loss,
-        "encoded_batch": batch,
-    }
-    return loss, info
+expert_encoder_loss = functools.partial(
+    _encoder_loss,
+    policy_loss_fn=lambda policy_logits: g_nonsaturating_softplus_loss(policy_logits),
+    domain_loss_fn=lambda domain_logits: g_nonsaturating_logistic_loss(domain_logits)
+)
