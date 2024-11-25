7f17ff6a458de021eef6eb33d63a61cbc11022a4
d-shlenskii <daniil.shlenskii@gmail.com>
2024-11-25 20:47:49 +0000

    evaluate util function extended: now returns trajectories if needed

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -6,12 +6,22 @@ import numpy as np
 from agents.base_agent import Agent
 
 
-def evaluate(agent: Agent, env: gym.Env, num_episodes: int, seed: int=0) -> Dict[str, float]:
+def evaluate(agent: Agent, env: gym.Env, num_episodes: int, seed: int=0, return_trajectories: bool=False) -> Dict[str, float]:
+    trajs = {"observations": [], "observations_next": []}
+
     env = gym.wrappers.RecordEpisodeStatistics(env, buffer_length=num_episodes)
     for i in range(num_episodes):
         observation, _, done, truncated = *env.reset(seed=seed+i), False, False
         while not (done or truncated):
             action = agent.eval_actions(observation)
-            observation, _, done, truncated, _ = env.step(action)
-
-    return {"return": np.mean(env.return_queue), "length": np.mean(env.length_queue)}
+            next_observation, _, done, truncated, _ = env.step(action)
+            
+            trajs["observations"].append(observation)
+            trajs["observations_next"].append(next_observation)
+            
+            observation = next_observation
+    
+    stats = {"return": np.mean(env.return_queue), "length": np.mean(env.length_queue)}
+    if return_trajectories:
+        return stats, trajs
+    return stats
