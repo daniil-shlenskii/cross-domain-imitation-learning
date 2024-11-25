6ee1ff91b3253ed4b57a710ae1e554b8b771b200
d-shlenskii <daniil.shlenskii@gmail.com>
2024-11-25 17:41:19 +0000

    agent.__init__ fixed

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,3 +1,3 @@
-from dida.cross_domain.cross_domain_dida_agent import CrossDomainDIDAAgent
-from gail.gail_agent import GAILAgent
-from sac.sac_agent import SACAgent
+from .dida.cross_domain.cross_domain_dida_agent import CrossDomainDIDAAgent
+from .gail.gail_agent import GAILAgent
+from .sac.sac_agent import SACAgent
