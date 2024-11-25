fd243298394803bc21f5bbc77d22d4e1f263b720
d-shlenskii <daniil.shlenskii@gmail.com>
2024-11-25 14:08:32 +0000

    base dida class added

--- agents/dida/utils.py
+++ agents/dida/utils.py
@@ -16,12 +16,6 @@ from utils.utils import get_buffer_state_size
 
 
 @jax.jit
-def process_policy_discriminator_input(x: jnp.ndarray):
-    doubled_b_size, dim = x.shape
-    x = x.reshape(2, doubled_b_size // 2, dim).transpose(1, 0, 2).reshape(-1, dim * 2)
-    return x
-
-@jax.jit
 def encode_observation_jit(encoder, observations):
     return encoder(observations)
 
