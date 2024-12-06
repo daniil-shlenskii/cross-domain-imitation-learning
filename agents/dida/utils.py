d749791b6cff1e3fc058676c3b810db00f1088a7
d-shlenskii <daniil.shlenskii@gmail.com>
2024-12-06 17:56:05 +0300

    dida: evlauate utils added

--- agents/dida/utils.py
+++ agents/dida/utils.py
@@ -1,62 +1,38 @@
-import functools
-import math
-from collections import defaultdict
-from copy import deepcopy
-from typing import Dict
-
-import gymnasium as gym
-import jax
-import jax.numpy as jnp
 import matplotlib.pyplot as plt
 import numpy as np
-from hydra.utils import instantiate
-from omegaconf.dictconfig import DictConfig
 from sklearn.manifold import TSNE
 
 from agents.base_agent import Agent
-from gan.discriminator import Discriminator
-from utils.evaluate import apply_model_jit, evaluate
-from utils.types import Buffer, BufferState, PRNGKey
-from utils.utils import get_buffer_state_size
+from utils import apply_model_jit
+from utils.types import BufferState, DataType
 
 
 def get_state_and_policy_tsne_scatterplots(
-    seed: int,
-    #
     dida_agent: Agent,
-    env: gym.Env,
-    num_episodes: int,
-    #
-    expert_buffer_state: BufferState,
-    anchor_buffer_state: BufferState,
+    seed: int,
+    learner_trajs: DataType,
 ):
     observation_keys = ["observations", "observations_next"]
 
-    # collect trajectories
-    _, learner_trajs = evaluate(
-        agent=dida_agent,
-        env=env,
-        num_episodes=num_episodes,
-        seed=seed,
-        return_trajectories=True
-    )
-    rollouts_size = learner_trajs["observations"].shape[0]
-    expert_trajs = {k: expert_buffer_state.experience[k][0, :rollouts_size] for k in observation_keys}
-    anchor_trajs = {k: anchor_buffer_state.experience[k][0, :rollouts_size] for k in observation_keys}
-    
+    # get trajectories
+    end_of_firt_traj_idx = np.argmax(learner_trajs["dones"])
+    learner_traj = {k: learner_trajs[k][0, :end_of_firt_traj_idx] for k in observation_keys}
+    expert_traj = {k: dida_agent.expert_buffer_state.experience[k][0, :end_of_firt_traj_idx] for k in observation_keys}
+    anchor_traj = {k: dida_agent.anchor_buffer_state.experience[k][0, :end_of_firt_traj_idx] for k in observation_keys}
+
     # encode trjectories
     for k in observation_keys:
-        learner_trajs[k] = encode_observation_jit(dida_agent.learner_encoder, learner_trajs[k])
-        expert_trajs[k] = encode_observation_jit(dida_agent.expert_encoder, expert_trajs[k])
-        anchor_trajs[k] = encode_observation_jit(dida_agent.expert_encoder, anchor_trajs[k])
+        learner_traj[k] = apply_model_jit(dida_agent.learner_encoder, learner_traj[k])
+        expert_traj[k] = apply_model_jit(dida_agent.expert_encoder, expert_traj[k])
+        anchor_traj[k] = apply_model_jit(dida_agent.expert_encoder, anchor_traj[k])
 
     # state and policy embeddings
-    learner_state_embeddings = learner_trajs["observations"]
-    expert_state_embeddings = expert_trajs["observations"]
+    learner_state_embeddings = learner_traj["observations"]
+    expert_state_embeddings = expert_traj["observations"]
 
-    learner_policy_embeddings = np.concatenate([learner_trajs["observations"], learner_trajs["observations_next"]], axis=1)
-    expert_policy_embeddings = np.concatenate([expert_trajs["observations"], expert_trajs["observations_next"]], axis=1)
-    anchor_policy_embeddings = np.concatenate([anchor_trajs["observations"], anchor_trajs["observations_next"]], axis=1)
+    learner_policy_embeddings = np.concatenate([learner_traj["observations"], learner_traj["observations_next"]], axis=1)
+    expert_policy_embeddings = np.concatenate([expert_traj["observations"], expert_traj["observations_next"]], axis=1)
+    anchor_policy_embeddings = np.concatenate([anchor_traj["observations"], anchor_traj["observations_next"]], axis=1)
 
     # combine embeddings for further processing
     state_embeddings_list = [
@@ -102,7 +78,7 @@ def get_state_and_policy_tsne_scatterplots(
         {"label": "SR", "c": "tab:orange", "marker": "s"},
     )
     figsize=(5, 5)
-    
+
     state_figure = plt.figure(figsize=figsize)
     for tsne_state_embeddings, scatter_params in zip(tsne_state_embeddings_list, scatter_params_list):
         plt.scatter(tsne_state_embeddings[:, 0], tsne_state_embeddings[:, 1], **scatter_params)
@@ -118,33 +94,20 @@ def get_state_and_policy_tsne_scatterplots(
     return state_figure, policy_figure
 
 def get_discriminators_hists(
-    seed: int,
-    #
     dida_agent: Agent,
-    env: gym.Env,
-    #
-    expert_buffer_state: BufferState
+    learner_trajs: DataType,
 ):
     observation_keys = ["observations", "observations_next"]
 
-    # learner trajectory
-    _, learner_traj = evaluate(
-        agent=dida_agent,
-        env=env,
-        num_episodes=1,
-        seed=seed,
-        return_trajectories=True
-    )
-
-    # expert trajectory
-    expert_exp = expert_buffer_state.experience
-    end_of_first_traj = np.argmax(expert_exp["dones"][0])
-    expert_traj = {k: expert_buffer_state.experience[k][0, :end_of_first_traj] for k in observation_keys}
+    # get trajectories
+    end_of_firt_traj_idx = np.argmax(learner_trajs["dones"])
+    learner_traj = {k: learner_trajs[k][0, :end_of_firt_traj_idx] for k in observation_keys}
+    expert_traj = {k: dida_agent.expert_buffer_state.experience[k][0, :end_of_firt_traj_idx] for k in observation_keys}
 
     # encode trjectories
     for k in observation_keys:
-        learner_traj[k] = encode_observation_jit(dida_agent.learner_encoder, learner_traj[k])
-        expert_traj[k] = encode_observation_jit(dida_agent.expert_encoder, expert_traj[k])
+        learner_traj[k] = apply_model_jit(dida_agent.learner_encoder, learner_traj[k])
+        expert_traj[k] = apply_model_jit(dida_agent.expert_encoder, expert_traj[k])
 
     # state and policy embeddings
     learner_state_embeddings = learner_traj["observations"]
