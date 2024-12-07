2a9574fb4a415157424a5aa2974e55ca2ef36ff6
d-shlenskii <daniil.shlenskii@gmail.com>
2024-12-07 19:51:30 +0300

    gail: sample discriminator added

--- agents/gail/gail_agent.py
+++ agents/gail/gail_agent.py
@@ -7,16 +7,21 @@ from flax import struct
 from hydra.utils import instantiate
 from omegaconf.dictconfig import DictConfig
 
+import wandb
 from agents.base_agent import Agent
 from agents.gail.gail_discriminator import GAILDiscriminator
-from utils import (get_buffer_state_size, instantiate_jitted_fbx_buffer,
-                   load_pickle)
+from gan.discriminator import Discriminator
+from utils import (convert_figure_to_array, get_buffer_state_size,
+                   instantiate_jitted_fbx_buffer, load_pickle)
 from utils.types import Buffer, BufferState, DataType
+
+from .utils import get_sample_discriminator_hists
 
 
 class GAILAgent(Agent):
     agent: Agent
     policy_discriminator: GAILDiscriminator
+    sample_discriminator: Discriminator
     expert_buffer: Buffer = struct.field(pytree_node=False)
     expert_buffer_state: BufferState = struct.field(pytree_node=False)
     n_policy_discriminator_updates: int = struct.field(pytree_node=False)
@@ -36,6 +41,7 @@ class GAILAgent(Agent):
         #
         agent_config: DictConfig,
         policy_discriminator_config: DictConfig,
+        sample_discriminator_config: DictConfig = None,
         #
         n_policy_discriminator_updates: int = 1,
         **kwargs,
@@ -57,6 +63,16 @@ class GAILAgent(Agent):
             input_dim=observation_dim * 2,
             _recursive_=False,
         )
+
+        if sample_discriminator_config is not None:
+            sample_discriminator = instantiate(
+                sample_discriminator_config,
+                seed=seed,
+                input_dim=observation_dim,
+                _recursive_=False,
+            )
+        else:
+            sample_discriminator = None
 
         # expert buffer init
         expert_buffer_state = load_pickle(expert_buffer_state_path)
@@ -91,6 +107,7 @@ class GAILAgent(Agent):
             expert_buffer_state=expert_buffer_state,
             agent=agent,
             policy_discriminator=policy_discriminator,
+            sample_discriminator=sample_discriminator,
             n_policy_discriminator_updates=n_policy_discriminator_updates,
             _save_attrs = _save_attrs,
             **kwargs,
@@ -109,6 +126,37 @@ class GAILAgent(Agent):
         )
         return new_gail_agent, info, stats_info
 
+    def evaluate(
+        self,
+        *,
+        seed: int,
+        env: gym.Env,
+        num_episodes: int,
+        #
+        convert_to_wandb_type: bool = True,
+        #
+        return_trajs: bool = False,
+    ):
+        eval_info, trajs = super().evaluate(seed=seed, env=env, num_episodes=num_episodes, return_trajectories=True)
+
+        # domain sample discriminator historgrams
+        (
+            state_learner_hist,
+            state_expert_hist,
+        ) = get_sample_discriminator_hists(
+            dida_agent=self,
+            learner_trajs=trajs,
+        )
+        if convert_to_wandb_type:
+            state_learner_hist = wandb.Image(convert_figure_to_array(state_learner_hist), caption="Sample Discriminator Learner logits")
+            state_expert_hist = wandb.Image(convert_figure_to_array(state_expert_hist), caption="Sample Discriminator Expert logits")
+        eval_info["sample_learner_hist"] = state_learner_hist
+        eval_info["sample_expert_hist"] = state_expert_hist
+
+        if return_trajs:
+            return eval_info, trajs
+        return eval_info
+
     @functools.partial(jax.jit, static_argnames="update_agent")
     def update_gail(
         self,
@@ -120,19 +168,29 @@ class GAILAgent(Agent):
         new_params = {}
 
         # update policy_discriminator
-        new_disc, info, stats_info = self.policy_discriminator.update(
+        new_domain_disc, info, stats_info = self.policy_discriminator.update(
             learner_batch=policy_discriminator_learner_batch,
             expert_batch=expert_batch,
         )
-        new_params["policy_discriminator"] = new_disc
+        new_params["policy_discriminator"] = new_domain_disc
 
         # update agent
         if update_agent:
-            batch["rewards"] = new_disc.get_rewards(batch)
+            batch["rewards"] = new_domain_disc.get_rewards(batch)
             new_agent, agent_info, agent_stats_info = self.agent.update(batch)
             new_params["agent"] = new_agent
             info.update(agent_info)
             stats_info.update(agent_stats_info)
+
+        # update sample discriminator if exists
+        if self.sample_discriminator is not None:
+            new_sample_disc, sample_disc_info, sample_disc_stats_info = self.sample_discriminator.update(
+                real_batch=expert_batch["observations"],
+                fake_batch=batch["observations"],
+            )
+            new_params["sample_discriminator"] = new_sample_disc
+            info.update(sample_disc_info)
+            stats_info.update(sample_disc_stats_info)
 
         new_gail_agent = self.replace(**new_params)
         return new_gail_agent, info, stats_info
