87f8e24055c5b7ac0b25445ac4032f2481f7b26b
d-shlenskii <daniil.shlenskii@gmail.com>
2024-12-09 03:38:12 +0300

    gail: sample_discriminator: ema_decay + state_encoder

--- agents/gail/sample_discriminator.py
+++ agents/gail/sample_discriminator.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 import jax
 import jax.numpy as jnp
 from flax import struct
@@ -8,8 +10,10 @@ from utils.types import DataType, PRNGKey
 
 class SampleDiscriminator(Discriminator):
     buffer_state_experience: DataType = struct.field(pytree_node=False)
+    state_encoder: Callable = struct.field(pytree_node=False)
     sample_size: int = struct.field(pytree_node=False)
     priorities: jnp.ndarray
+    ema_decay: float
     temperature: float
 
     @classmethod
@@ -18,29 +22,39 @@ class SampleDiscriminator(Discriminator):
         *,
         buffer_state_experience: DataType,
         sample_size: int,
+        state_encoder: Callable = None,
+        ema_decay: float = 0.99,
         temperature: float = 1.,
         **discriminator_kwargs,
     ):
+        if state_encoder is None:
+            state_encoder = lambda x: x
+
         exp_size = buffer_state_experience["observations"].shape[0]
         priorities = jnp.ones(exp_size) / float(exp_size)
 
         return super().create(
             buffer_state_experience=buffer_state_experience,
+            state_encoder = state_encoder,
             sample_size=sample_size,
             priorities=priorities,
+            ema_decay=ema_decay,
             temperature=temperature,
             info_key="sample_discriminator",
             _save_attrs = ("state", "priorities"),
             **discriminator_kwargs,
         )
 
-    def update(self, *, expert_batch: DataType, learner_batch: DataType):
-        new_sample_discriminator, info, stats_info = _update(
-            sample_discriminator=self,
+    def update(self, *, expert_batch: DataType, learner_batch: DataType, state_encoder: Callable=None):
+        new_sample_discr = self
+        if state_encoder is not None:
+            new_sample_discr = self.replace(state_encoder=state_encoder)
+        new_sample_discr, info, stats_info = _update(
+            sample_discriminator=new_sample_discr,
             learner_batch=learner_batch,
             expert_batch=expert_batch,
         )
-        return new_sample_discriminator, info, stats_info
+        return new_sample_discr, info, stats_info
 
     @jax.jit
     def sample(self, rng: PRNGKey):
@@ -61,8 +75,8 @@ class SampleDiscriminator(Discriminator):
         return new_rng, batch
 
     @jax.jit
-    def get_priorities(self):
-        states = self.buffer_state_experience["observations"]
+    def _get_priorities(self):
+        states = self.state_encoder(self.buffer_state_experience["observations"])
         logits = self(states)
         shifted_logits = logits - logits.min()
         normalized_logits =  shifted_logits / shifted_logits.max()
@@ -86,8 +100,8 @@ def _update(
     )
 
     # update priorities
-    priorities = new_sample_discr.get_priorities()
-    new_priorities = (priorities + sample_discriminator.priorities) * 0.5
+    priorities = new_sample_discr._get_priorities()
+    new_priorities = sample_discriminator.priorities * sample_discriminator.ema_decay + priorities * (1 - sample_discriminator.ema_decay)
     new_sample_discr = new_sample_discr.replace(priorities=new_priorities)
 
     return new_sample_discr, info, stats_info
