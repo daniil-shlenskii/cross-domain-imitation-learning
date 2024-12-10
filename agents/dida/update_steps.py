64961908257b8ef8e9165e6e842c4d051546232a
d-shlenskii <daniil.shlenskii@gmail.com>
2024-12-10 12:43:30 +0300

    gail/dida: _sample_expert_batch method

--- agents/dida/update_steps.py
+++ agents/dida/update_steps.py
@@ -39,13 +39,7 @@ def _update_encoders_and_domain_discriminator_jit(
     batch: DataType,
 ):
     # sample expert batch
-    new_rng, sample_discr_expert_batch = sample_batch(
-        dida_agent.rng, dida_agent.expert_buffer, dida_agent.expert_buffer_state
-    )
-    if dida_agent.sample_discriminator is not None:
-        new_rng, expert_batch = dida_agent.sample_discriminator.sample(new_rng)
-    else:
-        expert_batch = sample_discr_expert_batch
+    new_rng, expert_batch, sample_discr_expert_batch = dida_agent._sample_expert_batch()
     new_dida_agent = dida_agent.replace(rng=new_rng)
 
     # udpate encoders and domain discriminator
