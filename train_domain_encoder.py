3c2e4b7b48e9cab2e7928ad3d2a484174887a430
d-shlenskii <daniil.shlenskii@gmail.com>
2024-12-18 12:02:36 +0300

    train_domain_encoder.py: update

--- train_domain_encoder.py
+++ train_domain_encoder.py
@@ -4,21 +4,17 @@ from copy import deepcopy
 from pathlib import Path
 from typing import Dict
 
-import jax
-import numpy as np
-from gymnasium.wrappers import RescaleAction
 from hydra.utils import instantiate
 from loguru import logger
 from omegaconf import OmegaConf
 from tqdm import tqdm
 
 import wandb
-from utils import get_buffer_state_size, load_buffer, save_pickle
 
 
 def init() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
-        description="RL agent training script"
+        description="Training script"
     )
     parser.add_argument("--config",                type=str)
     parser.add_argument("--wandb_project",         type=str, default="_default_wandb_project_name")
@@ -30,25 +26,10 @@ def init() -> argparse.Namespace:
 def get_config_archive(config: Dict, config_path: str):
     config_archive = deepcopy(config.get("archive", {}))
 
-    default_agent_storage_dir = config_path[:-len(".yaml")]
-    default_random_buffer_storate_dir = "._tmp_archive_dir/random_buffers"
+    default_storage_dir = config_path[:-len(".yaml")]
 
-    config_archive["agent_load_dir"] = config_archive.get("agent_load_dir", default_agent_storage_dir) + "/domain_encoder"
-    config_archive["agent_save_dir"] = config_archive.get("agent_save_dir", default_agent_storage_dir) + "/domain_encoder"
-    config_archive["agent_buffer_load_dir"] = config_archive.get("agent_buffer_load_dir", default_agent_storage_dir)
-    config_archive["agent_buffer_save_dir"] = config_archive.get("agent_buffer_save_dir", default_agent_storage_dir)
-    config_archive["random_buffer_load_dir"] = config_archive.get("random_buffer_load_dir", default_random_buffer_storate_dir)
-    config_archive["random_buffer_save_dir"] = config_archive.get("random_buffer_save_dir", default_random_buffer_storate_dir)
-
-    for k, v in config_archive.items():
-        dir_path = Path(v)
-        dir_path.mkdir(exist_ok=True, parents=True)
-        config_archive[k] = dir_path
-
-    config_archive["agent_buffer_load_path"] = config_archive["agent_buffer_load_dir"] / "buffer.pickle"
-    config_archive["agent_buffer_save_path"] = config_archive["agent_buffer_save_dir"] / "buffer.pickle"
-    config_archive["random_buffer_load_path"] = config_archive["random_buffer_load_dir"] / f"{config.env_name}.pickle"
-    config_archive["random_buffer_save_path"] = config_archive["random_buffer_save_dir"] / f"{config.env_name}.pickle"
+    config_archive["load_dir"] = Path(config_archive.get("load_dir", default_storage_dir))
+    config_archive["save_dir"] = Path(config_archive.get("save_dir", default_storage_dir))
 
     return config_archive
 
@@ -59,125 +40,37 @@ def main(args: argparse.Namespace):
     ## process config part for saving/loading model
     config_archive = get_config_archive(config=config, config_path=args.config)
 
-    ## save config into agent dir
-    OmegaConf.save(config, config_archive["agent_save_dir"] / "config.yaml")
+    ## save config into model dir
+    OmegaConf.save(config, config_archive["save_dir"] / "config.yaml")
 
     # wandb logging init
-    wandb.init(project=args.wandb_project, dir=config_archive["agent_save_dir"])
+    wandb.init(project=args.wandb_project, dir=config_archive["save_dir"])
 
     logger.info(f"\nCONFIG:\n-------\n{OmegaConf.to_yaml(config)}")
 
-    # reprodicibility
-    rng = jax.random.PRNGKey(config.seed)
+    # model init
+    model = instantiate(config.model, _recursive_=False)
 
-    # environment init
-    env = instantiate(config.environment)
-    env = RescaleAction(env, -1, 1)
-
-    # agent init
-    agent = instantiate(config.agent, _recursive_=False)
-
-    ## load agent params if exist
-    if not args.from_scratch and config_archive["agent_load_dir"].exists():
-        agent, loaded_keys = agent.load(config_archive["agent_load_dir"])
+    ## load model gent params if exist
+    if not args.from_scratch and config_archive["load_dir"].exists():
+        model, loaded_keys = model.load(config_archive["load_dir"])
         logger.info(
-            f"Agent is initialized with data under the path: {config_archive['agent_load_dir']}.\n" + \
+            f"Model is initialized with data under the path: {config_archive['load_dir']}.\n" + \
             f"Loaded keys:\n----------------\n{OmegaConf.to_yaml(loaded_keys)}"
         )
-
-    # buffer init
-    observation, _ = env.reset()
-    action = env.action_space.sample()
-    observation, reward, done, _, _ = env.step(action)
-
-    buffer = instantiate(config.replay_buffer, _recursive_=False)
-    state = buffer.init(
-        dict(
-            observations=np.array(observation),
-            actions=np.array(action),
-            rewards=np.array(reward),
-            dones=np.array(done),
-            observations_next=np.array(observation),
-        )
-    )
-
-    ## load precollected agent buffer or collect random buffer
-    def do_environment_step(action, i):
-        nonlocal env, state, observation
-
-        # do step in the environment
-        observation_next, reward, done, truncated, _ = env.step(action)
-
-        # update buffer
-        state = buffer.add(
-            state,
-            dict(
-                observations=np.array(observation),
-                actions=np.array(action),
-                rewards=np.array(reward),
-                dones=np.array(done),
-                observations_next=np.array(observation_next),
-            )
-        )
-
-        observation = observation_next
-
-        # update env if terminated
-        if done or truncated:
-            observation, _ = env.reset(seed=config.seed + i)
-
-    ## precollect agent buffer
-    if not args.from_scratch and config_archive["agent_buffer_load_path"].exists():
-        # load precollected agent buffer
-        state = load_buffer(state, config_archive["agent_buffer_load_path"])
-        logger.info(f"Loading precollected Agent Buffer from {config_archive['agent_buffer_load_path']}.")
-    else:
-        # collect random buffer
-        ## download random buffer if given
-        n_iters_collect_buffer = config.precollect_buffer_size
-
-        if config_archive["random_buffer_load_path"].exists():
-            state = load_buffer(state, config_archive["random_buffer_load_path"], size=config.precollect_buffer_size)
-            n_iters_collect_buffer -= state.current_index
-            n_iters_collect_buffer = max(0, n_iters_collect_buffer)
-
-            logger.info(f"Loading Random Buffer from {config_archive['random_buffer_load_path']}.")
-            logger.info(f"{state.current_index} samples already collected. {n_iters_collect_buffer} are left.")
-
-        ## collect the rest amount of the data
-        if n_iters_collect_buffer > 0:
-            logger.info("Random Buffer collecting..")
-
-            observation, _  = env.reset(seed=config.seed)
-            for i in tqdm(range(n_iters_collect_buffer)):
-                action = env.action_space.sample()
-                do_environment_step(action, i)
-
-            logger.info("Random Buffer is collected.")
-
-        # save random buffer
-        save_pickle(state, config_archive["random_buffer_save_path"])
-        logger.info(f"Random Buffer is stored under the following path: {config_archive['random_buffer_save_path']}.")
-
-    logger.info(f"There are {get_buffer_state_size(state)} items in the Buffer.")
 
     # training
     logger.info("Training..")
 
-    observation, _  = env.reset(seed=config.seed)
     for i in tqdm(range(config.n_iters_training)):
-        # reproducibility
-        rng, agent_sample_key, buffer_sample_key = jax.random.split(rng, 3)
+        # evaluate model
+        if i == 0 or (i + 1) % config.eval_every == 0:
+            eval_info = model.evaluate(seed=config.evaluation.seed)
+            for k, v in eval_info.items():
+                wandb.log({f"evaluation/{k}": v}, step=i)
 
-        # sample actions
-        action = env.action_space.sample()
-
-        # do step in the environment
-        do_environment_step(action, i)
-
-        # do RL optimization step
-        batch = buffer.sample(state, buffer_sample_key).experience
-        agent, _, _, _, _, update_info, stats_info = agent.update(batch)
+        # optimization step
+        model, update_info, stats_info = model.update(None)
 
         # logging
         if (i + 1) % config.log_every == 0:
@@ -188,12 +81,9 @@ def main(args: argparse.Namespace):
 
         # save model
         if (i + 1) % config.save_every == 0:
-            agent.save(config_archive["agent_save_dir"])
-            save_pickle(state, config_archive["agent_buffer_save_path"])
+            model.save(config_archive["save_dir"])
 
-    logger.info(f"Agent is stored under the path: {config_archive['agent_save_dir']}")
-
-    env.close()
+    logger.info(f"Model is stored under the path: {config_archive['save_dir']}")
 
 
 if __name__ == "__main__":
