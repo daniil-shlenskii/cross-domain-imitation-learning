3f737bbb930c266bf0d5e9da8028ecfc1687a014
daniil-shlenskii <daniil.shlenskii@gmail.com>
2025-01-13 18:44:16 +0000

    register envs -> init

--- envs/custom_envs/__init__.py
+++ envs/custom_envs/__init__.py
@@ -6,3 +6,5 @@ from .toy_one_dim_env import ToyOneDimEnv, ToyOneDimEnvShifted
 def register_envs():
     gym.register(id="custom_envs/ToyOneDimEnv", entry_point=ToyOneDimEnv)
     gym.register(id="custom_envs/ToyOneDimEnvShifted", entry_point=ToyOneDimEnvShifted)
+
+register_envs()
