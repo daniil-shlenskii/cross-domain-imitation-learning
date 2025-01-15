2a731de1d862b7c258d3116b98709027bf383140
d-shlenskii <daniil.shlenskii@gmail.com>
2025-01-15 21:12:39 +0300

    gail_based enabled: commit what have been forgotten

--- agents/imitation_learning/cross_domain/gail_based/__init__.py
+++ agents/imitation_learning/cross_domain/gail_based/__init__.py
@@ -1,3 +1,1 @@
-from .dida_agent import DIDAAgent
-from .domain_encoder.cross_domain_encoder import CrossDomainEncoder
-from .domain_encoder.in_domain_encoder import InDomainEncoder
+from .gail_based_agent import GAILBasedAgent
