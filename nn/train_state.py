3ab5e2511b352b771ccdae97417e3da3a3847887
d-shlenskii <daniil.shlenskii@gmail.com>
2025-01-17 15:15:59 +0300

    train_state: grad_fn attr added

--- nn/train_state.py
+++ nn/train_state.py
@@ -11,29 +11,42 @@ from utils import SaveLoadFrozenDataclassMixin
 
 class TrainState(FlaxTrainState, SaveLoadFrozenDataclassMixin):
     loss_fn: Optional[Callable] = struct.field(pytree_node=False)
+    grad_fn: Optional[Callable] = struct.field(pytree_node=False)
     info_key: str = struct.field(pytree_node=False)
     _save_attrs: Tuple[str] = struct.field(pytree_node=False)
 
     @classmethod
     def create(
-        cls, *, apply_fn, params, tx, loss_fn: Callable, **kwargs
+        cls, *, apply_fn, params, tx, loss_fn: Callable = None, grad_fn: Callable = None, **kwargs
     ):
+        if grad_fn is None:
+            assert loss_fn is not None
+            def grad_fn(*, params, state, **loss_kwargs):
+                grads, info = jax.grad(loss_fn, has_aux=True)(
+                    params, state=state, **loss_kwargs
+                )
+                return grads, info
+
         return super().create(
             apply_fn=apply_fn,
             params=params,
             tx=tx,
             loss_fn=loss_fn,
+            grad_fn=grad_fn,
             _save_attrs=("step", "params", "opt_state"),
             **kwargs,
         )
+
+    def __getattribute__(self, item: str):
+        if self.loss_fn is None and item == "loss_fn":
+            return self.grad_fn
+        return super().__getattribute__(item)
 
     def __call__(self, *args, **kwargs):
         return self.apply_fn({"params": self.params}, *args, **kwargs)
 
     def update(self, **loss_kwargs):
-        grads, info = jax.grad(self.loss_fn, has_aux=True)(
-            self.params, state=self, **loss_kwargs
-        )
+        grads, info = self.grad_fn(self.params, state, **loss_kwargs)
 
         stats_info = {}
         stats_info[f"{self.info_key}/max_grad_norm"] = _compute_norms(grads)
