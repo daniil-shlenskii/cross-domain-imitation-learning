a5294c41602966ec87e66bdd2391e2e6ac68fa8b
d-shlenskii <daniil.shlenskii@gmail.com>
2025-01-18 03:25:43 +0300

    projected grad fixed

--- agents/imitation_learning/cross_domain/domain_encoder/one_domain_encoder/grad_fns/projected_grad.py
+++ agents/imitation_learning/cross_domain/domain_encoder/one_domain_encoder/grad_fns/projected_grad.py
@@ -12,9 +12,10 @@ class OneDomainEncoderProjectedGradPolicyToStateGradFn(OneDomainEncoderGradFn):
         return state_grad, random_policy_grad
 
     def process_source_grads(self, *, state_grad, random_policy_grad, expert_policy_grad):
-        policy_to_state_projection = jax.tree.map(project_a_to_b, expert_policy_grad, state_grad)
-        expert_policy_grad = jax.tree.map(lambda x, y: x - y, expert_policy_grad, policy_to_state_projection)
-        random_policy_grad = jax.tree.map(lambda x: x * 0., random_policy_grad)
+        expert_policy_to_state_projection = jax.tree.map(project_a_to_b, expert_policy_grad, state_grad)
+        expert_policy_grad = jax.tree.map(lambda x, y: x - y, expert_policy_grad, expert_policy_to_state_projection)
+        random_policy_to_state_projection = jax.tree.map(project_a_to_b, random_policy_grad, state_grad)
+        random_policy_grad = jax.tree.map(lambda x, y: x - y, random_policy_grad, random_policy_to_state_projection)
         return state_grad, random_policy_grad, expert_policy_grad
 
 class OneDomainEncoderProjectedGradStateToPolicyGradFn(OneDomainEncoderGradFn):
@@ -26,5 +27,4 @@ class OneDomainEncoderProjectedGradStateToPolicyGradFn(OneDomainEncoderGradFn):
     def process_source_grads(self, *, state_grad, random_policy_grad, expert_policy_grad):
         state_to_policy_projection = jax.tree.map(project_a_to_b, state_grad, expert_policy_grad)
         state_grad = jax.tree.map(lambda x, y: x - y, state_grad, state_to_policy_projection)
-        random_policy_grad = jax.tree.map(lambda x: x * 0., random_policy_grad)
         return state_grad, random_policy_grad, expert_policy_grad
