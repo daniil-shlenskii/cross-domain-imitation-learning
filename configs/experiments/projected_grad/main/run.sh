727b5e3013fe4a8136f06af0a46f0cdff7c8d55b
d-shlenskii <daniil.shlenskii@gmail.com>
2025-01-24 19:44:19 +0300

    proj_grad: run.sh

---
+++ configs/experiments/projected_grad/main/run.sh
@@ -0,0 +1,14 @@
+export CUDA_VISIBLE_DEVICES=2
+
+EXP_DIR=/Users/shlenskii/Desktop/github/cross-domain-imitation-learning/configs/experiments/projected_grad/main
+for N_DOMAINS in one_domain two_domains
+do
+    for PROJ_DIRECTION in state_to_policy policy_to_state 
+    do
+        for CONFIG_NAME in half_cheetah hopper
+        do
+            CONFIG_PATH=${EXP_DIR}/${N_DOMAINS}/${PROJ_DIRECTION}/${CONFIG_NAME}.yaml
+            python -m train_scripts.train_agent --config $CONFIG_PATH -w --from_scratch
+        done
+    done
+done
