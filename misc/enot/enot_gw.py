70eb5e38e1dc5367c80b333f5bdfdece58b91d27
d-shlenskii <daniil.shlenskii@gmail.com>
2025-03-06 20:07:34 +0300

    enot gw: structure changed

--- misc/enot/enot_gw.py
+++ misc/enot/enot_gw.py
@@ -1,71 +1,78 @@
-
 import jax
 import jax.numpy as jnp
-from flax import struct
+from hydra.utils import instantiate
+from omegaconf import DictConfig
 
+from .batch_preprocessors import BatchPreprocessor, IdentityPreprocessor
 from .enot import ENOT
 
 
 class ENOTGW(ENOT):
-    source_mean: jnp.ndarray
-    target_mean: jnp.ndarray
-    proj_matrix: jnp.ndarray
-    ema_decay: float = struct.field(pytree_node=False)
+    source_batch_preprocessor: BatchPreprocessor 
+    target_batch_preprocessor: BatchPreprocessor 
 
     @classmethod
     def create(
         cls,
         source_dim: int,
         target_dim: int,
-        ema_decay: float=0.99,
+        source_batch_preprocessor_config: DictConfig=None,
+        target_batch_preprocessor_config: DictConfig=None,
         **kwargs,
     ):
-        proj_matrix = jnp.zeros((target_dim, source_dim))
         kwargs["cost_fn_config"]["source_dim"] = source_dim
         kwargs["cost_fn_config"]["target_dim"] = target_dim
         if kwargs.get("train_cost_fn_config"):
             kwargs["train_cost_fn_config"]["source_dim"] = source_dim
             kwargs["train_cost_fn_config"]["target_dim"] = target_dim
 
+        if source_batch_preprocessor_config is not None:
+            source_batch_preprocessor = instantiate(source_batch_preprocessor_config, dim=source_dim)
+        else:
+            source_batch_preprocessor = IdentityPreprocessor.create()
+
+        if target_batch_preprocessor_config is not None:
+            target_batch_preprocessor = instantiate(target_batch_preprocessor_config, dim=target_dim)
+        else:
+            target_batch_preprocessor = IdentityPreprocessor.create()
+
         return super().create(
             source_dim=source_dim,
             target_dim=target_dim,
-            source_mean=jnp.zeros(source_dim),
-            target_mean=jnp.zeros(target_dim),
-            proj_matrix=proj_matrix,
-            ema_decay=ema_decay,
+            source_batch_preprocessor=source_batch_preprocessor,
+            target_batch_preprocessor=target_batch_preprocessor,
             **kwargs
         )
 
     @jax.jit
     def __call__(self, source: jnp.ndarray):
-        return self.transport(source - self.source_mean) + self.target_mean
+        source_encoded = self.source_batch_preprocessor.encode(source)
+        target_hat_encoded = self.transport(source_encoded)
+        target_hat = self.target_batch_preprocessor.decode(target_hat_encoded)
+        return target_hat
 
     def cost(self, source: jnp.ndarray, target: jnp.ndarray):
-        return jax.vmap(self.cost_fn)(source - self.source_mean, target - self.target_mean)
+        return jax.vmap(self.cost_fn)(
+           self.source_batch_preprocessor.encode(source),
+           self.target_batch_preprocessor.encode(target)
+        )
 
     @jax.jit
     def update(self, target: jnp.ndarray, source: jnp.ndarray):
-        # update means and cetralize batches
-        target, new_target_mean = self.preprocess_batch(target, self.target_mean)
-        source, new_source_mean = self.preprocess_batch(source, self.source_mean)
+        # preprocess batches
+        new_source_batch_preprocessor, source, _ = self.source_batch_preprocessor.update(source)
+        new_target_batch_preprocessor, target, _ = self.target_batch_preprocessor.update(target)
 
         # update cost fn
-        new_proj_matrix = self.update_proj_matrix(target, source)
-        new_cost_fn = self.cost_fn.replace(proj_matrix=new_proj_matrix)
-        self = self.replace(proj_matrix=new_proj_matrix, cost_fn=new_cost_fn)
+        new_cost_fn = self.cost_fn.update(source=source, target=target)
+        new_train_cost_fn = self.train_cost_fn.update(source=source, target=target)
+        self = self.replace(
+            target_batch_preprocessor=new_target_batch_preprocessor,
+            source_batch_preprocessor=new_source_batch_preprocessor,
+            cost_fn=new_cost_fn,
+            train_cost_fn=new_train_cost_fn
+        )
 
         enot, info, stats_info = super().update(target, source)
 
-        enot = enot.replace(target_mean=new_target_mean, source_mean=new_source_mean)
-
         return enot, info, stats_info
-
-    def preprocess_batch(self, batch: jnp.ndarray, mean: jnp.ndarray):
-        new_mean = mean * self.ema_decay + batch.mean(0) * (1 - self.ema_decay)
-        batch = batch - new_mean
-        return batch, new_mean
-
-    def update_proj_matrix(self, target: jnp.ndarray, source: jnp.ndarray):
-        proj_matrix_opt = 4 * jax.vmap(lambda x, y: x[:, None] * y[None, :])(target, source).mean(0)
-        return self.proj_matrix * self.ema_decay + proj_matrix_opt * (1 - self.ema_decay)
