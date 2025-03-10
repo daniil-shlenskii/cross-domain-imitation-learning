e640abd68a5ebff882ca84fe40f5b0f135e031cf
d-shlenskii <daniil.shlenskii@gmail.com>
2025-03-11 00:18:09 +0300

    pairs cost added

--- misc/enot/costs.py
+++ misc/enot/costs.py
@@ -11,7 +11,7 @@ class GWCost(PyTreeNode):
     @classmethod
     def create(cls, source_dim: int, target_dim: int, ema_decay: float=0.99, **kwargs):
         proj_matrix = jnp.eye(target_dim, source_dim)
-        return cls(proj_matrix=proj_matrix, ema_decay=ema_decay, **kwargs)
+        return cls(proj_matrix=proj_matrix, ema_decay=ema_decay)
 
     def __call__(self, x, y):
         add1 = (y * (self.proj_matrix @ x)).sum()
@@ -45,7 +45,7 @@ class InnerGWCost(PyTreeNode):
     @classmethod
     def create(cls, source_dim: int, target_dim: int, ema_decay: float=0.99, **kwargs):
         proj_matrix = jnp.eye(target_dim, source_dim)
-        return cls(source_dim=source_dim, proj_matrix=proj_matrix, ema_decay=ema_decay, **kwargs)
+        return cls(source_dim=source_dim, proj_matrix=proj_matrix, ema_decay=ema_decay)
 
     def __call__(self, x, y):
         return -(y * (self.proj_matrix @ x)).sum()
@@ -60,3 +60,29 @@ class InnerGWCost(PyTreeNode):
 class InnerGWCostStable(InnerGWCost):
     def __call__(self, x, y):
         return jnp.linalg.norm(self.proj_matrix @ x - y)**2
+
+class PairsCost(PyTreeNode):
+    state_cost: InnerGWCostStable
+    pairs_cost: InnerGWCostStable
+
+    def __getattr__(self, name):
+        return self.pairs_cost.__getattribute__(name)
+
+    @classmethod
+    def create(cls, source_dim: int, target_dim: int, ema_decay: float=0.99, **kwargs):
+        state_cost = InnerGWCostStable.create(source_dim=source_dim//2, target_dim=target_dim//2, ema_decay=ema_decay)
+        pairs_cost = InnerGWCostStable.create(source_dim=source_dim, target_dim=target_dim, ema_decay=ema_decay)
+        return cls(state_cost=state_cost, pairs_cost=pairs_cost)
+
+    def __call__(self, x_pairs, y_pairs):
+        x = jnp.split(x_pairs, 2, axis=-1)[0]
+        y = jnp.split(y_pairs, 2, axis=-1)[0]
+        return self.state_cost(x, y) + self.pairs_cost(x_pairs, y_pairs)
+
+    def update(self, *, source: jnp.ndarray, target: jnp.ndarray):
+        source_pairs, target_pairs = source, target
+        source = jnp.split(source_pairs, 2, axis=-1)[0] 
+        target = jnp.split(target_pairs, 2, axis=-1)[0]
+        new_state_cost = self.state_cost.update(source=source, target=target)
+        new_pairs_cost = self.pairs_cost.update(source=source_pairs, target=target_pairs)
+        return self.replace(state_cost=new_state_cost, pairs_cost=new_pairs_cost)
