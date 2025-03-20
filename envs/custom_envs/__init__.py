4aa310adf8df77090109114b439c3fcfc57e92d9
d-shlenskii <daniil.shlenskii@gmail.com>
2025-03-20 12:14:26 +0300

    custom point env added

--- envs/custom_envs/__init__.py
+++ envs/custom_envs/__init__.py
@@ -3,12 +3,13 @@ import gymnasium_robotics
 
 gym.register_envs(gymnasium_robotics)
 
+from .custom_point_umaze import CustomPointUmaze
 from .toy_one_dim_env import ToyOneDimEnv, ToyOneDimEnvShifted
 
 
 def register_envs():
     gym.register(id="custom_envs/ToyOneDimEnv", entry_point=ToyOneDimEnv)
     gym.register(id="custom_envs/ToyOneDimEnvShifted", entry_point=ToyOneDimEnvShifted)
+    gym.register(id="CustomPointUmaze", entry_point=CustomPointUmaze)
 
 register_envs()
-
---
+++ envs/custom_envs/custom_point_umaze.py
@@ -0,0 +1,19 @@
+from gymnasium_robotics.envs.maze.point_maze import PointMazeEnv
+
+
+class CustomPointUmaze(PointMazeEnv):
+    def __init__(
+        self,
+        reward_type="dense",
+        render_mode=None,
+    ):
+        super().__init__(
+            reward_type=reward_type,
+            render_mode = render_mode,
+            continuing_task = True,
+            reset_target = False,
+        )
+
+    def reset(self, *args, seed=None, **kwargs):
+        options = {"goal_cell": (1, 1), "reset_cell": (3, 1)}
+        return super().reset(seed=seed, options=options)
