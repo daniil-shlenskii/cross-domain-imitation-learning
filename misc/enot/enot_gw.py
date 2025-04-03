ea9a64298c88ffa359f09225fdf39b42a1f40372
d-shlenskii <daniil.shlenskii@gmail.com>
2025-04-03 13:31:08 +0300

    gwot: remove use_projection option

--- misc/enot/enot_gw.py
+++ misc/enot/enot_gw.py
@@ -9,14 +9,12 @@ from .utils import mapping_scatter
 
 
 class ENOTGW(ENOT):
-    use_projection: bool
 
     @classmethod
     def create(
         cls,
         source_dim: int,
         target_dim: int,
-        use_projection: bool=False,
         **kwargs,
     ):
         kwargs["cost_fn_config"]["source_dim"] = source_dim
@@ -25,37 +23,12 @@ class ENOTGW(ENOT):
             kwargs["train_cost_fn_config"]["source_dim"] = source_dim
             kwargs["train_cost_fn_config"]["target_dim"] = target_dim
 
-        enot = super().create(
+        return super().create(
             source_dim=source_dim,
             target_dim=target_dim,
             use_projection=use_projection,
             **kwargs
         )
-
-        if use_projection:
-            transport_loss = enot.transport.loss_fn
-            def new_transport_loss(params, state, source, enot):
-                proj_matrix = enot.train_cost_fn.proj_matrix
-                source = jax.vmap(lambda x: proj_matrix @ x)(source)
-                return transport_loss(params, state, source, enot)
-            new_transport = enot.transport.replace(loss_fn=new_transport_loss)
-            enot = enot.replace(transport=new_transport)
-        return enot
-
-    @jax.jit
-    def __call__(self, source: jnp.ndarray):
-        source_encoded = self.source_batch_preprocessor.encode(source)
-
-        source_encoded = jax.lax.cond(
-            self.use_projection,
-            lambda x: jax.vmap(lambda x: self.train_cost_fn.proj_matrix @ x)(x),
-            lambda x: x,
-            source_encoded
-        )
-
-        target_hat_encoded = self.transport(source_encoded)
-        target_hat = self.target_batch_preprocessor.decode(target_hat_encoded)
-        return target_hat
 
     def evaluate(self, source: jnp.ndarray, target: jnp.ndarray, convert_to_wandb_type: bool=True):
         eval_info = super().evaluate(source, target, convert_to_wandb_type=convert_to_wandb_type)
